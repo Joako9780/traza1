d3ca9e0f92ddd881c6544b4547a89500cb73cd6c
Joako9780 <57472877+Joako9780@users.noreply.github.com>
2025-09-30 16:53:50 -0300

    Modificación de Empresa
    Se añadieron atributos, su relación y se usó Lombok

--- src/main/java/org/example/entidades/Empresa.java
+++ src/main/java/org/example/entidades/Empresa.java
@@ -1,4 +1,22 @@
 package org.example.entidades;
 
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class Empresa {
+    private String nombre;
+    private String razonSocial;
+    private Integer cuit;
+    private String logo;
+
+    @Builder.Default
+    private Set<Sucursal> sucursales = new HashSet<>();  //Set no permite elementos repetidos
 }
